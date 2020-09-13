d194cbeec7b1c878af91aa5ea99dd208806a2bd5
ezer1025 <enoshzerahia@gmail.com>
2020-09-13 12:22:50 +0300

    Moving from constant array to Vector

--- src/net/handler.rs
+++ src/net/handler.rs
@@ -1,5 +1,3 @@
-use crate::defaults;
-
 pub struct LoginHandler {}
 
 pub struct ChannelHandler {}
@@ -7,35 +5,23 @@ pub struct ChannelHandler {}
 pub struct WorldHandler {}
 
 pub trait GenericHandler {
-    fn handle(
-        &self,
-        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
-    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE];
+    fn handle(&self, buffer: Vec<u8>, buffer_size: usize) -> (Vec<u8>, usize);
 }
 
 impl GenericHandler for LoginHandler {
-    fn handle(
-        &self,
-        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
-    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
-        return buffer;
+    fn handle(&self, buffer: Vec<u8>, buffer_size: usize) -> (Vec<u8>, usize) {
+        return (buffer, buffer_size);
     }
 }
 
 impl GenericHandler for ChannelHandler {
-    fn handle(
-        &self,
-        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
-    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
-        return buffer;
+    fn handle(&self, buffer: Vec<u8>, buffer_size: usize) -> (Vec<u8>, usize) {
+        return (buffer, buffer_size);
     }
 }
 
 impl GenericHandler for WorldHandler {
-    fn handle(
-        &self,
-        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
-    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
-        return buffer;
+    fn handle(&self, buffer: Vec<u8>, buffer_size: usize) -> (Vec<u8>, usize) {
+        return (buffer, buffer_size);
     }
 }
