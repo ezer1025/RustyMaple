4fd17ac93d71da9df2ef6d0af07647a822ea3061
ezer1025 <enoshzerahia@gmail.com>
2020-09-13 12:14:15 +0300

    Referencing packet

--- src/net/handler.rs
+++ src/net/handler.rs
@@ -1,3 +1,5 @@
+use crate::defaults;
+
 pub struct LoginHandler {}
 
 pub struct ChannelHandler {}
@@ -5,17 +7,35 @@ pub struct ChannelHandler {}
 pub struct WorldHandler {}
 
 pub trait GenericHandler {
-    fn handle(&self);
+    fn handle(
+        &self,
+        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
+    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE];
 }
 
 impl GenericHandler for LoginHandler {
-    fn handle(&self) {}
+    fn handle(
+        &self,
+        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
+    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
+        return buffer;
+    }
 }
 
 impl GenericHandler for ChannelHandler {
-    fn handle(&self) {}
+    fn handle(
+        &self,
+        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
+    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
+        return buffer;
+    }
 }
 
 impl GenericHandler for WorldHandler {
-    fn handle(&self) {}
+    fn handle(
+        &self,
+        buffer: [u8; defaults::DEFAULT_BUFFER_SIZE],
+    ) -> [u8; defaults::DEFAULT_BUFFER_SIZE] {
+        return buffer;
+    }
 }
