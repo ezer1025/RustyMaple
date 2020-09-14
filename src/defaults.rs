f0a0cb07f39b6450e7adc2fcceb3387217ca6eba
ezer1025 <enoshzerahia@gmail.com>
2020-09-14 15:07:17 +0300

    added new constants

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,3 +1,9 @@
 pub const DEFAULT_CLIENT_WORKERS: usize = 10;
 pub const DEFAULT_CLIENTS_THREADS: usize = 100;
-pub const DEFAULT_BUFFER_SIZE: usize = 1024;
+pub const DEFAULT_HEADER_LENGTH: usize = 4;
+
+// constants
+pub const MAPLESTORY_LOCALE: u8 = 8;
+pub const MAPLESTORY_VERSION: u16 = 55;
+pub const MAPLESTORY_SUBVERSION: &str = "1";
+pub const INITIALIZE_VECTORS_SIZE: usize = 4;
