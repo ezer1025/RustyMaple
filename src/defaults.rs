9d645e15afa6c47d7f2abcab5e28dd4e7d1e7ef0
ezer1025 <enoshzerahia@gmail.com>
2020-09-20 00:37:28 +0300

    AES constants

--- src/defaults.rs
+++ src/defaults.rs
@@ -6,4 +6,6 @@ pub const DEFAULT_HEADER_LENGTH: usize = 4;
 pub const MAPLESTORY_LOCALE: u8 = 8;
 pub const MAPLESTORY_VERSION: u16 = 55;
 pub const MAPLESTORY_SUBVERSION: &str = "1";
-pub const INITIALIZE_VECTORS_SIZE: usize = 4;
+pub const USER_SEQUENCE_SIZE: usize = 4;
+pub const AES_KEY_SIZE: usize = 32;
+pub const AES_BLOCK_SIZE: usize = 16;
