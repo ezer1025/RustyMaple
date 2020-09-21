bc481334dd78dba7a7e32b2e6eeba49ae021e21d
enoshzer <enoshzerahia@gmail.com>
2020-09-21 09:54:07 +0300

    Move adding header to encrypted packets only

--- src/net/client.rs
+++ src/net/client.rs
@@ -167,21 +167,23 @@ impl Client {
                 Ok(sendable_message) => {
                     let mut final_buffer = sendable_message.buffer;
 
-                    let packet_header = crypto::generate_packet_header(
-                        final_buffer.len() as u16,
-                        &user_send_sequence,
-                        &defaults::MAPLESTORY_VERSION,
-                    );
-
-                    match stream.write(&packet_header[..]) {
-                        Ok(_) => debug!("written packet header to {}", stream.peer_addr().unwrap()),
-                        Err(error) => {
-                            warn!("could not write to TcpStream [{}]", error);
-                            continue;
-                        }
-                    }
-
                     if sendable_message.encrypted {
+                        let packet_header = crypto::generate_packet_header(
+                            final_buffer.len() as u16,
+                            &user_send_sequence,
+                            &defaults::MAPLESTORY_VERSION,
+                        );
+
+                        match stream.write(&packet_header[..]) {
+                            Ok(_) => {
+                                debug!("written packet header to {}", stream.peer_addr().unwrap())
+                            }
+                            Err(error) => {
+                                warn!("could not write to TcpStream [{}]", error);
+                                continue;
+                            }
+                        };
+
                         final_buffer = match crypto::maple_custom_encrypt(
                             &final_buffer,
                             &mut user_send_sequence,
