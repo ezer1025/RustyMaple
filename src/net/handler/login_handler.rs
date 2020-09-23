8fa85877910ed38a3433d38ffa608bffd3479181
enoshzer <enoshzerahia@gmail.com>
2020-09-23 17:26:09 +0300

    Added login handler

--- src/net/handler/login_handler.rs
+++ src/net/handler/login_handler.rs
@@ -5,11 +5,30 @@ pub struct LoginHandler {}
 
 impl GenericHandler for LoginHandler {
     fn handle(&self, buffer: Vec<u8>, buffer_size: usize) -> (Vec<u8>, usize) {
-        let mut bytes = &buffer[..];
+        let mut bytes = &buffer.clone()[..];
 
-        match bytes.get_u16() {
+        match bytes.get_u16_le() {
+            0x1bu16 => Self::login(&mut bytes),
             _ => (),
         };
         return (buffer, buffer_size);
+    }
+}
+
+impl LoginHandler {
+    fn login(buffer: &mut &[u8]) {
+        let username_length = buffer.get_u16_le();
+        let mut username = vec![0u8; username_length as usize];
+        buffer.copy_to_slice(&mut username);
+
+        let password_length = buffer.get_u16_le();
+        let mut password = vec![0u8; password_length as usize];
+        buffer.copy_to_slice(&mut password);
+
+        println!(
+            "user {}:{} tried to login",
+            String::from_utf8(username).unwrap(),
+            String::from_utf8(password).unwrap()
+        );
     }
 }
